f4b46c694a81c70abb157694d6eec2654cc29d33
ryanakann <ryanakann@gmail.com>
2021-04-01 18:25:37 -0400

    Phage particle update

--- College Jam/Assets/Scripts/Graphs/Nodes/NodeUI.cs
+++ College Jam/Assets/Scripts/Graphs/Nodes/NodeUI.cs
@@ -13,6 +13,7 @@ namespace Graphs
             public NodeSelection nodeSelection;
             public TMP_Text basicUI;
             public GameObject detailedUI;
+            public GameObject phagePrefab;
 
             private void Start()
             {
@@ -22,6 +23,20 @@ namespace Graphs
             public void SetDetailVisibility(bool visibile)
             {
                 detailedUI.SetActive(visibile);
+            }
+
+            public void SendPhages(Node node, int quantity)
+            {
+                StartCoroutine(SendPhageCR(node, quantity));
+            }
+
+            IEnumerator SendPhageCR(Node node, int quantity)
+            {
+                for (int i = 0; i < quantity; i++)
+                {
+                    Instantiate(phagePrefab);
+                    yield return new WaitForSeconds(0.3f);
+                }
             }
         }
     }
