2d6e93392e39d81e28c6101269f3fcd3d12f86ab
ryanakann <ryanakann@gmail.com>
2021-04-02 02:00:13 -0400

    mUSIC HEHEHEHEH

--- College Jam/Assets/Scripts/MusicManager.cs
+++ College Jam/Assets/Scripts/MusicManager.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class Music
+    {
+        public string name;
+        public AudioClip clip;
+        public float duration;
+    }
+
     public static MusicManager instance;
+
+    public UnityEngine.Audio.AudioMixerGroup group;
+    public List<Music> music;
+    private Dictionary<string, Music> musicMap;
+    private AudioSource source1;
+    private AudioSource source2;
+    private int activeSource;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -17,11 +33,39 @@ public class MusicManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        musicMap = new Dictionary<string, Music>();
+        foreach (var item in music)
+        {
+            musicMap.Add(item.name, item);
+        }
+        source1 = AddAudioSource();
+        source2 = AddAudioSource();
+
+        activeSource = 0;
+        Play(music[0]);
     }
 
-    // Update is called once per frame
-    void Update()
+    private AudioSource AddAudioSource()
     {
-        
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+        source.outputAudioMixerGroup = group;
+        source.playOnAwake = false;
+        source.loop = false;
+        return source;
+    }
+
+    public void Play(Music music)
+    {
+        StartCoroutine(PlayCR(music));
+    }
+
+    IEnumerator PlayCR(Music music)
+    {
+        AudioSource source = activeSource++ % 2 == 0 ? source1 : source2;
+        source.clip = music.clip;
+        source.Play();
+        yield return new WaitForSeconds(music.duration);
+        Play(music);
     }
 }
